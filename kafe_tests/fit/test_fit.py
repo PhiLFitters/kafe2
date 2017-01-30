d76cf84c04f57c3160facd65e687b4f7d4b2de39
Daniel Savoiu <daniel.savoiu@cern.ch>
2017-01-30 13:56:55 +0100

    Create separate module for unittests. Re-commit unittests in new hierarchy.

---
+++ kafe_tests/fit/test_fit.py
@@ -0,0 +1,153 @@
+import unittest
+
+import numpy as np
+
+from kafe.core.nexus import Nexus
+from kafe.fit import NexusFitter
+
+from kafe.fit.minimizers.iminuit_minimizer import MinimizerIMinuit
+
+
+# class TestNexusFitter(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.ps = Nexus()
+#         self.n_points = 10
+#
+#         self.x_support = np.arange(self.n_points)
+#         self.y_true_errors = np.array([-0.14828884, -0.01484907, -1.07937053,
+#                                        -0.16996879,  1.48169694, -0.24361865,
+#                                         1.80168599, -0.80596291,  1.64451791,
+#                                        -1.05440662])
+#         self.y_measured = np.arange(self.n_points) + self.y_true_errors
+#         self.y_error_model = np.ones(self.n_points) * 1.0
+#
+#         self.ps.new(x_support=self.x_support)
+#         self.ps.new(y_measured=self.y_measured)
+#         self.ps.new(y_errors=self.y_error_model)
+#
+#         self.slope = 1.0
+#         self.y_intercept = 0.0
+#         self.chi2_initial = 11.182777169656681
+#         self.ps.new(slope=self.slope)
+#         self.ps.new(y_intercept=self.y_intercept)
+#
+#         def y_theory(x_support, slope, y_intercept):
+#             return slope * x_support + y_intercept
+#
+#         def chi2(y_measured, y_theory, y_errors):
+#             _p = y_measured - y_theory
+#             return np.sum((_p / y_errors) ** 2)
+#
+#         self.ps.new_function(y_theory)
+#         self.ps.new_function(chi2)
+#
+#         self.sf = NexusFitter(self.ps, ('slope', 'y_intercept'), 'chi2')
+#         self.sf_only_slope = NexusFitter(self.ps, ('slope',), 'chi2')
+#
+#         self.slope_after_fit = 1.0546496459587062
+#         self.y_intercept_after_fit = -0.10477992306698165
+#         self.chi2_after_fit = 10.737168784133402
+#
+#         self.slope_after_fit_only_slope = 1.0381054758557233
+#         self.chi2_after_fit_only_slope = 10.768949646411523
+#
+#     def test_compare_par_values_initial(self):
+#         self.assertDictEqual(self.sf.fit_parameter_values, dict(slope=self.slope, y_intercept=self.y_intercept))
+#
+#     def test_compare_par_values_after_fit(self):
+#         self.sf.do_fit()
+#         self.assertDictEqual(self.sf.fit_parameter_values, dict(slope=self.slope_after_fit, y_intercept=self.y_intercept_after_fit))
+#
+#     def test_compare_par_values_after_fit_only_slope(self):
+#         self.sf_only_slope.do_fit()
+#         self.assertDictEqual(self.sf_only_slope.fit_parameter_values, dict(slope=self.slope_after_fit_only_slope))
+#
+#     def test_compare_chi2_value_initial(self):
+#         self.assertEqual(self.sf.parameter_to_minimize_value, self.chi2_initial)
+#
+#     def test_compare_chi2_value_after_fit(self):
+#         self.sf.do_fit()
+#         self.assertEqual(self.sf.parameter_to_minimize_value, self.chi2_after_fit)
+#
+#     def test_compare_chi2_value_after_fit_only_slope(self):
+#         self.sf_only_slope.do_fit()
+#         self.assertEqual(self.sf_only_slope.parameter_to_minimize_value, self.chi2_after_fit_only_slope)
+
+class TestNexusFitterIMinuit(unittest.TestCase):
+
+    def setUp(self):
+        self.ps = Nexus()
+        self.n_points = 10
+
+        self.x_support = np.arange(self.n_points)
+        self.y_true_errors = np.array([-0.14828884, -0.01484907, -1.07937053,
+                                       -0.16996879,  1.48169694, -0.24361865,
+                                        1.80168599, -0.80596291,  1.64451791,
+                                       -1.05440662])
+        self.y_measured = np.arange(self.n_points) + self.y_true_errors
+        self.y_error_model = np.ones(self.n_points) * 1.0
+
+        self.ps.new(x_support=self.x_support)
+        self.ps.new(y_measured=self.y_measured)
+        self.ps.new(y_errors=self.y_error_model)
+
+        self.slope = 1.0
+        self.y_intercept = 0.0
+        self.chi2_initial = 11.182777169656681
+        self.ps.new(slope=self.slope)
+        self.ps.new(y_intercept=self.y_intercept)
+
+        def y_theory(x_support, slope, y_intercept):
+            return slope * x_support + y_intercept
+
+        def chi2(y_measured, y_theory, y_errors):
+            _p = y_measured - y_theory
+            return np.sum((_p / y_errors) ** 2)
+
+        self.ps.new_function(y_theory)
+        self.ps.new_function(chi2)
+
+        self.sf = NexusFitter(self.ps, ('slope', 'y_intercept'), 'chi2', minimizer_class=MinimizerIMinuit)
+        self.sf_only_slope = NexusFitter(self.ps, ('slope',), 'chi2', minimizer_class=MinimizerIMinuit)
+
+        self.slope_after_fit = 1.0546496459587062
+        self.y_intercept_after_fit = -0.10477992306698165
+        self.chi2_after_fit = 10.737168784133402
+
+        self.slope_after_fit_only_slope = 1.0381054758557233
+        self.chi2_after_fit_only_slope = 10.768949646411523
+
+    def test_compare_par_values_initial(self):
+        self.assertDictEqual(self.sf.fit_parameter_values, dict(slope=self.slope, y_intercept=self.y_intercept))
+
+    def test_compare_par_values_after_fit(self):
+        self.sf.do_fit()
+        _ref_dict = dict(slope=self.slope_after_fit, y_intercept=self.y_intercept_after_fit)
+        _chk_dict = self.sf.fit_parameter_values
+        self.assertEqual(set(_ref_dict.keys()), set(_chk_dict.keys()))
+        for _pn in _chk_dict:
+            self.assertAlmostEqual(_chk_dict[_pn],
+                                   _ref_dict[_pn],
+                                   places=2)
+
+    def test_compare_par_values_after_fit_only_slope(self):
+        self.sf_only_slope.do_fit()
+        _ref_dict = dict(slope=self.slope_after_fit_only_slope)
+        _chk_dict = self.sf_only_slope.fit_parameter_values
+        self.assertEqual(set(_ref_dict.keys()), set(_chk_dict.keys()))
+        for _pn in _chk_dict:
+            self.assertAlmostEqual(_chk_dict[_pn],
+                                   _ref_dict[_pn],
+                                   places=2)
+
+    def test_compare_chi2_value_initial(self):
+        self.assertAlmostEqual(self.sf.parameter_to_minimize_value, self.chi2_initial, places=4)
+
+    def test_compare_chi2_value_after_fit(self):
+        self.sf.do_fit()
+        self.assertAlmostEqual(self.sf.parameter_to_minimize_value, self.chi2_after_fit, places=4)
+
+    def test_compare_chi2_value_after_fit_only_slope(self):
+        self.sf_only_slope.do_fit()
+        self.assertAlmostEqual(self.sf_only_slope.parameter_to_minimize_value, self.chi2_after_fit_only_slope, places=4)
---
+++ kafe_tests/test_cov_mat.py
@@ -0,0 +1,105 @@
+import unittest
+import numpy as np
+
+from kafe.error import CovMat, cov_mat_from_float_list, cov_mat_from_float
+
+
+class TestCovMat(unittest.TestCase):
+
+    def setUp(self):
+        cm = CovMat([
+            [1.00000000e-02,  1.80600000e-02,  8.19000000e-03,  5.62800000e-02,  1.26000000e-03],
+            [1.80600000e-02,  1.84900000e-01,  3.52170000e-02,  2.42004000e-01,  5.41800000e-03],
+            [8.19000000e-03,  3.52170000e-02,  3.80250000e-02,  1.09746000e-01,  2.45700000e-03],
+            [5.62800000e-02,  2.42004000e-01,  1.09746000e-01,  1.79560000e+00,  1.68840000e-02],
+            [1.26000000e-03,  5.41800000e-03,  2.45700000e-03,  1.68840000e-02,  9.00000000e-04]])
+        self.reference = [2., 1., 3., 2., 9.]
+        self.cm = cm
+
+
+    def test_rescale(self):
+        self.cm.rescale(self.reference, [3,2,1,9,2])
+        _ref = np.array([
+            [  2.25000000e-02,   5.41800000e-02,   4.09500000e-03,   3.79890000e-01,    4.20000000e-04],
+            [  5.41800000e-02,   7.39600000e-01,   2.34780000e-02,   2.17803600e+00,    2.40800000e-03],
+            [  4.09500000e-03,   2.34780000e-02,   4.22500000e-03,   1.64619000e-01,    1.82000000e-04],
+            [  3.79890000e-01,   2.17803600e+00,   1.64619000e-01,   3.63609000e+01,    1.68840000e-02],
+            [  4.20000000e-04,   2.40800000e-03,   1.82000000e-04,   1.68840000e-02,    4.44444444e-05]])
+        self.assertTrue(np.allclose(self.cm.mat, _ref))
+
+    def test_inverse(self):
+        self.assertTrue(np.allclose(self.cm.I.dot(self.cm.mat), np.eye(self.cm.mat.shape[0])))
+
+    def test_split_svd(self):
+        self.assertTrue(np.allclose(self.cm.mat, np.sum(self.cm.split_svd(), axis=0)))
+
+
+
+class TestCovMatHelperFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.values = [0.1, 0.43, 0.195, 1.34, 0.03]
+
+        self.ref_fullcor = np.array([
+            [1.00000000e-02,  4.30000000e-02,  1.95000000e-02,  1.34000000e-01,  3.00000000e-03],
+            [4.30000000e-02,  1.84900000e-01,  8.38500000e-02,  5.76200000e-01,  1.29000000e-02],
+            [1.95000000e-02,  8.38500000e-02,  3.80250000e-02,  2.61300000e-01,  5.85000000e-03],
+            [1.34000000e-01,  5.76200000e-01,  2.61300000e-01,  1.79560000e+00,  4.02000000e-02],
+            [3.00000000e-03,  1.29000000e-02,  5.85000000e-03,  4.02000000e-02,  9.00000000e-04]])
+
+        self.ref_nocor = np.array([
+            [1.00000000e-02,             0.0,             0.0,             0.0,             0.0],
+            [           0.0,  1.84900000e-01,             0.0,             0.0,             0.0],
+            [           0.0,             0.0,  3.80250000e-02,             0.0,             0.0],
+            [           0.0,             0.0,             0.0,  1.79560000e+00,             0.0],
+            [           0.0,             0.0,             0.0,             0.0,  9.00000000e-04]])
+
+        self.ref_partcor = np.array([
+            [1.00000000e-02,  1.80600000e-02,  8.19000000e-03,  5.62800000e-02,  1.26000000e-03],
+            [1.80600000e-02,  1.84900000e-01,  3.52170000e-02,  2.42004000e-01,  5.41800000e-03],
+            [8.19000000e-03,  3.52170000e-02,  3.80250000e-02,  1.09746000e-01,  2.45700000e-03],
+            [5.62800000e-02,  2.42004000e-01,  1.09746000e-01,  1.79560000e+00,  1.68840000e-02],
+            [1.26000000e-03,  5.41800000e-03,  2.45700000e-03,  1.68840000e-02,  9.00000000e-04]])
+
+        self.single_value = 0.123
+
+        self.ref_singleval_fullcor = np.array([
+            [0.015129, 0.015129, 0.015129],
+            [0.015129, 0.015129, 0.015129],
+            [0.015129, 0.015129, 0.015129]])
+
+        self.ref_singleval_nocor = np.array([
+            [ 0.015129,  0.      ,  0.      ],
+            [ 0.      ,  0.015129,  0.      ],
+            [ 0.      ,  0.      ,  0.015129]])
+
+        self.ref_singleval_partcor = np.array([
+            [0.01512900,  0.00635418,  0.00635418],
+            [0.00635418,  0.01512900,  0.00635418],
+            [0.00635418,  0.00635418,  0.01512900]])
+
+
+    def test_cov_mat_from_float_list_nocor(self):
+        cm = cov_mat_from_float_list(self.values, correlation=0.0)
+        self.assertTrue(np.allclose(cm.mat, self.ref_nocor))
+
+    def test_cov_mat_from_float_list_partialcor(self):
+        cm = cov_mat_from_float_list(self.values, correlation=0.42)
+        self.assertTrue(np.allclose(cm.mat, self.ref_partcor))
+
+    def test_cov_mat_from_float_list_fullcor(self):
+        cm = cov_mat_from_float_list(self.values, correlation=1.0)
+        self.assertTrue(np.allclose(cm.mat, self.ref_fullcor))
+
+    def test_cov_mat_from_float_nocor(self):
+        cm = cov_mat_from_float(self.single_value, 3, correlation=0.0)
+        self.assertTrue(np.allclose(cm.mat, self.ref_singleval_nocor))
+
+    def test_cov_mat_from_float_partcor(self):
+        cm = cov_mat_from_float(self.single_value, 3, correlation=0.42)
+        self.assertTrue(np.allclose(cm.mat, self.ref_singleval_partcor))
+
+    def test_cov_mat_from_float_fullcor(self):
+        cm = cov_mat_from_float(self.single_value, 3, correlation=1.0)
+        self.assertTrue(np.allclose(cm.mat, self.ref_singleval_fullcor))
+
