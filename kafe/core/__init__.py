e6ad36ea90bbd2822ad20c364a926713c253841d
Daniel Savoiu <daniel.savoiu@cern.ch>
2017-02-03 14:09:22 +0100

    Prevent loading ROOT minimizer if ROOT Python bindings are not available

--- kafe/core/__init__.py
+++ kafe/core/__init__.py
@@ -5,11 +5,16 @@ Core API: core components for fitting with kafe
 """
 
 from fitters import Nexus, NexusFitter
-from minimizers import MinimizerROOTTMinuit, MinimizerIMinuit, MinimizerScipyOptimize
+from minimizers import MinimizerIMinuit, MinimizerScipyOptimize
 #from fitters import NexusCython
 
 __all__ = ['Nexus',
            'NexusFitter',
-           'MinimizerROOTTMinuit',
            'MinimizerIMinuit',
            'MinimizerScipyOptimize']
+
+try:
+    from minimizers import MinimizerROOTTMinuit
+    __all__.append('MinimizerROOTTMinuit')
+except ImportError:
+    pass
