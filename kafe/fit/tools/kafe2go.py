9d523986296dc5e89d8ea6dc84e640a7e0a88a1a
Johannes Gäßler <updrn@student.kit.edu>
2018-06-29 12:04:53 +0200

    Implemented a simple, unsafe, functional first version of kafe2go
    kafe2go can interpret kwargs from a string and construct an XYFit
    object.
    Any inserted Python code will be executed as the model function.

--- kafe/fit/tools/kafe2go.py
+++ kafe/fit/tools/kafe2go.py
@@ -1,5 +1,16 @@
 import yaml
+from ..xy import XYFit
+
+def _construct_xy_fit(loader, node):
+    _kwargs = loader.construct_mapping(node, deep=True)
+    #TODO sanitize
+    exec _kwargs["model_function"]
+    _kwargs["model_function"] = model_function
+    return XYFit(**_kwargs)
+
+def add_constructors():
+    yaml.add_constructor("!xyfit", _construct_xy_fit)
 
 def yaml_to_fit(input_string):
-    print yaml.dump(yaml.load(input_string))
+    return yaml.load(input_string)
 
--- kafe_tests/fit/test_kafe2go.py
+++ kafe_tests/fit/test_kafe2go.py
@@ -1,15 +1,27 @@
 import unittest
-from kafe.fit.tools import yaml_to_fit
+import numpy as np
+from kafe.fit.tools import kafe2go
 
 class TestYAMLToFitObject(unittest.TestCase):
     
     def setUp(self):
-        pass
+        kafe2go.add_constructors()
     
     def test_string_to_fit_object(self):
-        input_string="""
-        !!python/object:kafe.fit.XYFit
-        xy_data: 0.0
-        model_function: 0.0
+        _input_string="""
+        !xyfit
+        xy_data: [[0.0, 1.0, 2.0, 3.0, 4.0, 5.0], [-1.0, 1.0, 3.0, 5.0, 7.0, 9.0]]
+        model_function:
+            >
+            def model_function(x, a , b):
+                return a * x + b
         """
-        yaml_to_fit(input_string)
+        _xy_fit = kafe2go.yaml_to_fit(_input_string)
+        _xy_fit.do_fit()
+        self.assertTrue(
+            np.allclose(
+                _xy_fit.parameter_values,
+                [2.0, -1.0],
+                rtol=1e-3
+            )
+        )
