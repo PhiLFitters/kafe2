7819b46f7a2ea48d7c4526c83d89e927856cb8a2
Johannes Gäßler <updrn@student.kit.edu>
2018-06-30 13:46:08 +0200

    Implemented constructors for HistFit, IndexedFit + test cases

--- kafe/fit/tools/kafe2go.py
+++ kafe/fit/tools/kafe2go.py
@@ -1,19 +1,32 @@
 import yaml
+from ..histogram import HistFit
+from ..indexed import IndexedFit
 from ..xy import XYFit
 
-def _construct_xy_fit(loader, node):
+def _construct_fit(fit_type, model_function_kwarg_name, loader, node):
     _kwargs = loader.construct_mapping(node, deep=True)
     __locals_pointer = [None] #TODO better solution?
-    _exec_string = _kwargs["model_function"]
+    _exec_string = _kwargs[model_function_kwarg_name]
     _exec_string += "\n__locals_pointer[0] = locals()"
     exec(_exec_string, {"__builtins__":{"locals":locals}, "__locals_pointer":__locals_pointer})
     _locals = __locals_pointer[0]
     del _locals["__builtins__"]
     del _locals["__locals_pointer"]
-    _kwargs["model_function"] = _locals.values()[0]
-    return XYFit(**_kwargs)
+    _kwargs[model_function_kwarg_name] = _locals.values()[0] #TODO adjust for multifits
+    return fit_type(**_kwargs)
+
+def _construct_hist_fit(loader, node):
+    return _construct_fit(HistFit, "model_density_function", loader, node)
+
+def _construct_indexed_fit(loader, node):
+    return _construct_fit(IndexedFit, "model_function", loader, node)
+
+def _construct_xy_fit(loader, node):
+    return _construct_fit(XYFit, "model_function", loader, node)
 
 def add_constructors():
+    yaml.add_constructor("!histfit", _construct_hist_fit)
+    yaml.add_constructor("!indexedfit", _construct_indexed_fit)
     yaml.add_constructor("!xyfit", _construct_xy_fit)
 
 def yaml_to_fit(input_string):
--- kafe_tests/fit/test_kafe2go.py
+++ kafe_tests/fit/test_kafe2go.py
@@ -3,6 +3,7 @@ import numpy as np
 import logging
 import sys
 from kafe.fit.tools import kafe2go
+from kafe.fit import HistFit, IndexedFit, XYFit
 
 class TestYAMLToFitObject(unittest.TestCase):
     
@@ -12,9 +13,26 @@ class TestYAMLToFitObject(unittest.TestCase):
         stream_handler = logging.StreamHandler(sys.stdout)
         logger.addHandler(stream_handler)
         kafe2go.add_constructors()
-    
-    def test_string_to_fit_object(self):
-        _input_string="""
+        self._input_string_hist="""
+        !histfit
+        data: [-0.32523704 -0.83835927  0.13349729 -1.63891736  0.62477429  0.44002838
+                1.57854108 -2.26540303 -2.26564588 -0.65707497  0.54147452 -1.14683608
+               -0.40344333  1.19959957  0.94797447 -0.62652507  0.77217068  0.86948057
+               -0.63931895  0.80411386]
+        model_density_function:
+            >
+            def linear_model(x, a , b):
+                return a * x + b
+        """
+        self._input_string_indexed="""
+        !indexedfit
+        data: [-1.0, 1.0, 3.0, 5.0, 7.0, 9.0]
+        model_function:
+            >
+            def linear_model(x, a , b):
+                return a * x + b
+        """
+        self._input_string_xy="""
         !xyfit
         xy_data: [[0.0, 1.0, 2.0, 3.0, 4.0, 5.0], [-1.0, 1.0, 3.0, 5.0, 7.0, 9.0]]
         model_function:
@@ -22,7 +40,23 @@ class TestYAMLToFitObject(unittest.TestCase):
             def linear_model(x, a , b):
                 return a * x + b
         """
-        _xy_fit = kafe2go.yaml_to_fit(_input_string)
+
+    
+    def test_string_to_hist_fit(self):
+        _hist_fit = kafe2go.yaml_to_fit(self._input_string_hist)
+        self.assertTrue(isinstance(_hist_fit, HistFit))
+    
+    def test_string_to_indexed_fit(self):
+        _indexed_fit = kafe2go.yaml_to_fit(self._input_string_indexed)
+        print _indexed_fit
+        self.assertTrue(isinstance(_indexed_fit, IndexedFit))
+    
+    def test_string_to_xy_fit(self):
+        _xy_fit = kafe2go.yaml_to_fit(self._input_string_xy)
+        self.assertTrue(isinstance(_xy_fit, XYFit))
+    
+    def test_string_to_xy_fit_do_fit(self):
+        _xy_fit = kafe2go.yaml_to_fit(self._input_string_xy)
         _xy_fit.do_fit()
         self.assertTrue(
             np.allclose(
