f4965a8b5678631fa6f32ac682fa8b0c7a766b0f
Johannes Gäßler <updrn@student.kit.edu>
2018-07-27 17:50:13 +0200

    Arg formatter yaml repr now used in model function formatter yaml repr
    Made the _make_represenation/_make_object methods static to allow use
    from outside the Writer/Reader classes, needed for hierarchical
    writing/reading.

--- kafe_tests/fit/test_representers_format_yaml.py
+++ kafe_tests/fit/test_representers_format_yaml.py
@@ -14,8 +14,24 @@ model_function_formatter:
     latex_name: quadratic model
     x_name: x
     latex_x_name: x
-    expression_string: {0} * {x} ** 2 + {1} * {x} + {2}
-    latex_expression_string: {0}{x}^2 + {1}{x} + {2} 
+    arg_formatters:
+        - model_parameter_formatter:
+            name: a
+            value: 1.1
+            error: 0.1
+            latex_name: a
+        - model_parameter_formatter:
+            name: b
+            value: 2.2
+            error: 0.1
+            latex_name: b
+        - model_parameter_formatter:
+            name: c
+            value: 3.3
+            error: 0.1
+            latex_name: c
+    expression_string: '{0} * {x} ** 2 + {1} * {x} + {2}'
+    latex_expression_string: '{0}{x}^2 + {1}{x} + {2}' 
 """
 
 TEST_MODEL_PARAMETER_FORMATTER = """
@@ -35,9 +51,9 @@ class TestXYModelFunctionFormatterYamlRepresenter(unittest.TestCase):
                 x_name='x',
                 latex_x_name='x',
                 arg_formatters=[
-                    ModelParameterFormatter(name='a', value='1.1', error=None, latex_name='a'),
-                    ModelParameterFormatter(name='b', value='2.2', error=None, latex_name='b'),
-                    ModelParameterFormatter(name='c', value='3.3', error=None, latex_name='c')
+                    ModelParameterFormatter(name='a', value=1.1, error=0.1, latex_name='a'),
+                    ModelParameterFormatter(name='b', value=2.2, error=0.1, latex_name='b'),
+                    ModelParameterFormatter(name='c', value=3.3, error=0.1, latex_name='c')
                 ],
                 expression_string='{0} * {x} ** 2 + {1} * {x} + {2}',
                 latex_expression_string='{0}{x}^2 + {1}{x} + {2}' 
@@ -52,6 +68,40 @@ class TestXYModelFunctionFormatterYamlRepresenter(unittest.TestCase):
     
     def test_write_to_roundtrip_stringstream(self):
         self._roundtrip_streamwriter.write()
+
+    def test_read_from_testfile_stream(self):
+        _read_model_function_formatter = self._testfile_streamreader.read()
+        self.assertTrue(isinstance(_read_model_function_formatter, XYModelFunctionFormatter))
+        
+        self.assertTrue(_read_model_function_formatter.name == self._model_function_formatter.name)
+        self.assertTrue(_read_model_function_formatter.latex_name == self._model_function_formatter.latex_name)
+        self.assertTrue(_read_model_function_formatter._x_name == self._model_function_formatter._x_name)
+        for _read_arg_formatter, _given_arg_formatter in zip(_read_model_function_formatter._arg_formatters, 
+                                                             self._model_function_formatter._arg_formatters):
+            self.assertTrue(_read_arg_formatter.name == _given_arg_formatter.name)
+            self.assertTrue(_read_arg_formatter.value == _given_arg_formatter.value)
+            self.assertTrue(_read_arg_formatter.error == _given_arg_formatter.error)
+            self.assertTrue(_read_arg_formatter.latex_name == _given_arg_formatter.latex_name)
+        self.assertTrue(_read_model_function_formatter.expression_format_string == self._model_function_formatter.expression_format_string)
+        self.assertTrue(_read_model_function_formatter.latex_expression_format_string == self._model_function_formatter.latex_expression_format_string)
+
+    def test_round_trip_with_stringstream(self):
+        self._roundtrip_streamwriter.write()
+        self._roundtrip_stringstream.seek(0)  # return to beginning
+        _read_model_function_formatter = self._roundtrip_streamreader.read()
+        self.assertTrue(isinstance(_read_model_function_formatter, XYModelFunctionFormatter))
+
+        self.assertTrue(_read_model_function_formatter.name == self._model_function_formatter.name)
+        self.assertTrue(_read_model_function_formatter.latex_name == self._model_function_formatter.latex_name)
+        self.assertTrue(_read_model_function_formatter._x_name == self._model_function_formatter._x_name)
+        for _read_arg_formatter, _given_arg_formatter in zip(_read_model_function_formatter._arg_formatters, 
+                                                             self._model_function_formatter._arg_formatters):
+            self.assertTrue(_read_arg_formatter.name == _given_arg_formatter.name)
+            self.assertTrue(_read_arg_formatter.value == _given_arg_formatter.value)
+            self.assertTrue(_read_arg_formatter.error == _given_arg_formatter.error)
+            self.assertTrue(_read_arg_formatter.latex_name == _given_arg_formatter.latex_name)
+        self.assertTrue(_read_model_function_formatter.expression_format_string == self._model_function_formatter.expression_format_string)
+        self.assertTrue(_read_model_function_formatter.latex_expression_format_string == self._model_function_formatter.latex_expression_format_string)
 
 class TestXYArgumentFormatterYamlRepresenter(unittest.TestCase):
 
@@ -74,21 +124,21 @@ class TestXYArgumentFormatterYamlRepresenter(unittest.TestCase):
         self._roundtrip_streamwriter.write()
 
     def test_read_from_testfile_stream(self):
-        _read_model_function = self._testfile_streamreader.read()
-        self.assertTrue(isinstance(_read_model_function, ModelParameterFormatter))
+        _read_model_parameter_formatter = self._testfile_streamreader.read()
+        self.assertTrue(isinstance(_read_model_parameter_formatter, ModelParameterFormatter))
         
-        self.assertTrue(_read_model_function.name == self._model_parameter_formatter.name)
-        self.assertTrue(_read_model_function.value == self._model_parameter_formatter.value)
-        self.assertTrue(_read_model_function.error == self._model_parameter_formatter.error)
-        self.assertTrue(_read_model_function.latex_name == self._model_parameter_formatter.latex_name)
+        self.assertTrue(_read_model_parameter_formatter.name == self._model_parameter_formatter.name)
+        self.assertTrue(_read_model_parameter_formatter.value == self._model_parameter_formatter.value)
+        self.assertTrue(_read_model_parameter_formatter.error == self._model_parameter_formatter.error)
+        self.assertTrue(_read_model_parameter_formatter.latex_name == self._model_parameter_formatter.latex_name)
 
     def test_round_trip_with_stringstream(self):
         self._roundtrip_streamwriter.write()
         self._roundtrip_stringstream.seek(0)  # return to beginning
-        _read_model_function = self._roundtrip_streamreader.read()
-        self.assertTrue(isinstance(_read_model_function, ModelParameterFormatter))
+        _read_parameter_formatter = self._roundtrip_streamreader.read()
+        self.assertTrue(isinstance(_read_parameter_formatter, ModelParameterFormatter))
 
-        self.assertTrue(_read_model_function.name == self._model_parameter_formatter.name)
-        self.assertTrue(_read_model_function.value == self._model_parameter_formatter.value)
-        self.assertTrue(_read_model_function.error == self._model_parameter_formatter.error)
-        self.assertTrue(_read_model_function.latex_name == self._model_parameter_formatter.latex_name)
+        self.assertTrue(_read_parameter_formatter.name == self._model_parameter_formatter.name)
+        self.assertTrue(_read_parameter_formatter.value == self._model_parameter_formatter.value)
+        self.assertTrue(_read_parameter_formatter.error == self._model_parameter_formatter.error)
+        self.assertTrue(_read_parameter_formatter.latex_name == self._model_parameter_formatter.latex_name)
