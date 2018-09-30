c83be62719c0d4ed19b5ec3b7143912f91a8e3e4
Johannes Gäßler <updrn@student.kit.edu>
2018-09-30 12:46:38 +0200

    Added function_library module contained in new kafe.util package

---
+++ kafe/util/__init__.py
@@ -0,0 +1,9 @@
+r"""
+.. module:: kafe.fit.util
+    :platform: Unix
+    :synopsis: This submodule provides utility functions for other modules
+
+.. moduleauthor:: Johannes Gaessler <johannes.gaessler@student.kit.edu>
+"""
+
+from .function_library import *
