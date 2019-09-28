2ddc2d4ce342da54dfd5edf5aded8f19bf7b9f5f
Daniel Savoiu <daniel.savoiu@cern.ch>
2019-09-28 15:36:19 +0200

    [NexusFitter] Adapt to new implementation of `Nexus`.

--- kafe2/core/fitters/nexus_fitter.py
+++ kafe2/core/fitters/nexus_fitter.py
@@ -12,68 +12,64 @@ class NexusFitter(object):
 
     def __init__(self, nexus, parameters_to_fit, parameter_to_minimize, minimizer=None, minimizer_kwargs=None):
         self._nx = nexus
+
         self.parameters_to_fit = parameters_to_fit
         self.parameter_to_minimize = parameter_to_minimize
-
-        # local cache
-        self.__cache_stale = True
-        self.__cache_all_parameters_name_value_dict = None
-        self.__cache_fit_parameters_name_value_dict = None
-        self.__cache_parameter_to_minimize_value = None
-
-        self.__minimizing = False
-        _par_name_val_map = self.fit_parameter_values
-        if minimizer_kwargs == None:
-            minimizer_kwargs = dict()
 
         if minimizer is not None:
             _minimizer_class = get_minimizer(minimizer)
         else:
             _minimizer_class = get_minimizer()  # get the default minimizer
 
-        self._minimizer = _minimizer_class(parameters_to_fit,
-                                           list(_par_name_val_map.values()),
-                                           [0.1 if _v==0 else 0.1*_v for _v in _par_name_val_map.values()],
-                                           self._fcn_wrapper, **minimizer_kwargs)
+        if minimizer_kwargs is None:
+            minimizer_kwargs = dict()
+
+        # initialize minimizer
+        _par_name_val_map = self.fit_parameter_values
+        _par_values = [_par_name_val_map[_pn] for _pn in parameters_to_fit]
+        self._minimizer = _minimizer_class(
+            parameters_to_fit,
+            _par_values,
+            [0.1 if _v==0 else 0.1*_v for _v in _par_values],
+            self._fcn_wrapper,
+            **minimizer_kwargs
+        )
+
+        # flags
+        self.__minimizing = False  # minimization ongoing?
         self.__state_is_from_minimizer = False
 
 
     # -- private methods
 
-    def _check_parnames_in_par_space_raise(self, fit_pars):
-        for _pn in fit_pars:
-            if self._nx.get_by_name(_pn) is None:
-                raise NexusFitterException("No parameter with name '%s' registered in Nexus (%r)!" % (_pn, self._nx))
+    def _get_pars_from_nexus(self, par_names):
+        '''return list of nexus nodes for `par_names`'''
+        _not_found = []
+        _pars = []
+        for _pn in par_names:
+            _par = self._nx.get(_pn)
+            if _par is None:
+                _not_found.append(_pn)
+            else:
+                _pars.append(_par)
 
-    def _renew_all_par_cache(self):
-        _fpns = self.parameters_to_fit
-        _apnvd = self.__cache_all_parameters_name_value_dict = self._nx.parameter_values_dict
-        self.__cache_fit_parameters_name_value_dict = OrderedDict([(_pn, _apnvd[_pn]) for _pn in _fpns])
-        self.__cache_parameter_to_minimize_value = _apnvd[self.parameter_to_minimize]
 
-    def _renew_fit_par_cache(self):
-        _fpns = self.parameters_to_fit
-        _fpvs = self._nx.get_values_by_name(_fpns)
-        self.__cache_fit_parameters_name_value_dict = OrderedDict([(_pn, _pv) for _pn, _pv in zip(_fpns, _fpvs)])
-
-    def _renew_par_to_minimize_cache(self):
-        _ptmn = self.parameter_to_minimize
-        _ptmv = self._nx.get_values_by_name(_ptmn)
-        self.__cache_parameter_to_minimize_value = _ptmv
+        if _not_found:
+            raise NexusFitterException(
+                "Parameters not registered in Nexus: {}".format(
+                    ', '.join(_not_found)
+                )
+            )
+        return _pars
 
     def _minimize(self, max_calls=None):
-        self.__minimizing = True
+        '''run minimizer'''
+
         if max_calls is None:
             max_calls = kc('core', 'fitters', 'nexus_fitter', 'max_calls')
+
+        self.__minimizing = True
         self._minimizer.minimize(max_calls=max_calls)
-        # opt.minimize(self._fcn_wrapper,
-        #              self.fit_parameter_values.values(),
-        #              args=(), method=None, jac=None,
-        #              hess=None, hessp=None,
-        #              bounds=None, constraints=(),
-        #              tol=None,
-        #              callback=None,
-        #              options=None)
         self.__minimizing = False
 
         # evaluate function one more time with the final parameters,
@@ -82,20 +78,17 @@ class NexusFitter(object):
         self._fcn_wrapper(*_par_vals)
 
         self.__state_is_from_minimizer = True
-        self.__cache_stale = True
-
 
     def _fcn_wrapper(self, *fit_par_value_list):
-        # # set parameters to current values
-        # if not (len(fit_par_value_list) == 1 and fit_par_value_list[0] is None):
-        #     if not len(fit_par_value_list) == self.n_fit_par:
-        #         print fit_par_value_list
-        #         print self.n_fit_par
-        # set parameters to current values
-        _par_val_dict = {_pn: _pv for _pn, _pv in zip(self.parameters_to_fit, fit_par_value_list)}
-        self._nx.set(**_par_val_dict)
+
+        # set fit parameter values
+        assert(len(fit_par_value_list) == len(self._fit_pars))
+        for _par, _new_value in zip(self._fit_pars, fit_par_value_list):
+            _par.value = _new_value
+
         # evaluate function and return value
-        return self._nx.get_values_by_name(self.parameter_to_minimize)
+        return self._min_par.value
+
 
     # -- public properties
 
@@ -105,28 +98,29 @@ class NexusFitter(object):
 
     @property
     def parameters_to_fit(self):
-        return self._fit_pars
+        return self._fit_par_names
 
     @parameters_to_fit.setter
     def parameters_to_fit(self, fit_parameters):
-        self._check_parnames_in_par_space_raise(fit_parameters)
-        self._fit_pars = fit_parameters
+        self._fit_pars = self._get_pars_from_nexus(fit_parameters)
+        self._fit_par_names = tuple(fit_parameters)
 
     @property
     def parameter_to_minimize(self):
-        return self._parameter_to_minimize
+        return self._min_par_name
 
     @parameter_to_minimize.setter
     def parameter_to_minimize(self, parameter_to_minimize):
-        self._check_parnames_in_par_space_raise((parameter_to_minimize,))
-        self._parameter_to_minimize = parameter_to_minimize
+        self._min_par = \
+            self._get_pars_from_nexus([parameter_to_minimize])[0]
+        self._min_par_name = parameter_to_minimize
 
     @property
     def fit_parameter_values(self):
-        # if self.__cache_stale or self.__minimizing:  # allow getting fresh (non-cached) parameters while minimizing
-        if self.__cache_stale:
-            self._renew_fit_par_cache()
-        return self.__cache_fit_parameters_name_value_dict
+        return {
+            _pn : self._nx.get(_pn).value
+            for _pn in self._fit_par_names
+        }
 
     @property
     def fit_parameter_cov_mat(self):
@@ -150,10 +144,7 @@ class NexusFitter(object):
 
     @property
     def parameter_to_minimize_value(self):
-        # if self.__cache_stale or self.__minimizing:  # allow getting fresh (non-cached) parameters while minimizing
-        if self.__cache_stale:
-            self._renew_par_to_minimize_cache()
-        return self.__cache_parameter_to_minimize_value
+        return self._nx.get(self._min_par_name).value
 
     @property
     def n_fit_par(self):
@@ -185,35 +176,60 @@ class NexusFitter(object):
 
     def contour(self, parameter_name_1, parameter_name_2, sigma=1.0, **kwargs):
         if not self.__state_is_from_minimizer:
-            raise NexusFitterException("To calculate a contour the do_fit method has to be called first.")
+            raise NexusFitterException(
+                "To calculate a contour the do_fit method has to be called first."
+            )
+
         return self._minimizer.contour(parameter_name_1, parameter_name_2, sigma=sigma, **kwargs)
 
     def profile(self, parameter_name, bins=20, bound=2, args=None, subtract_min=False):
         if not self.__state_is_from_minimizer:
-            raise NexusFitterException("To calculate a profile the do_fit method has to be called first.")
+            raise NexusFitterException(
+                "To calculate a profile the do_fit method has to be called first."
+            )
+
         return self._minimizer.profile(parameter_name, bins=bins, bound=bound, subtract_min=subtract_min)
 
     def set_fit_parameter_values(self, **parameter_value_dict):
         _dict_key_set = set(parameter_value_dict.keys())
         _par_name_set = set(self.parameters_to_fit)
+
         # test parameter names
         if not _dict_key_set.issubset(_par_name_set):
             _unknown_par_names = _dict_key_set - _par_name_set
             raise NexusFitterException("Cannot set fit parameter values: Unknown fit parameters: %r!"
                                        % (_unknown_par_names,))
+
         # set values in nexus
-        self._nx.set(**parameter_value_dict)
-        self._minimizer.set_several(list(parameter_value_dict.keys()), list(parameter_value_dict.values()))
+        for _par_name, _new_value in parameter_value_dict.items():
+            self._nx.get(_par_name).value = _new_value
+            self._minimizer.set(_par_name, _new_value)
+
         # set flags
         self.__state_is_from_minimizer = False
-        self.__cache_stale = True
 
     def set_all_fit_parameter_values(self, fit_par_value_list):
         # test list length
         if not len(fit_par_value_list) == len(self.parameters_to_fit):
-            raise NexusFitterException("Cannot set all fit parameter values: %d fit parameters declared, "
-                                       "but %d provided!"
-                                       % (len(self.parameters_to_fit), len(fit_par_value_list)))
-        # set values in nexus
-        _par_val_dict = {_pn: _pv for _pn, _pv in zip(self.parameters_to_fit, fit_par_value_list)}
-        self.set_fit_parameter_values(**_par_val_dict)
+            raise NexusFitterException(
+                "Cannot set all fit parameter values: "
+                "{} fit parameters declared, "
+                "but {} provided!".format(
+                    self.n_fit_par,
+                    len(fit_par_value_list)
+                )
+            )
+
+        # set values in nexus and minimizer
+        for _par_name, _par, _new_value in zip(self._fit_par_names, self._fit_pars, fit_par_value_list):
+            _par.value = _new_value
+            self._minimizer.set(_par_name, _new_value)
+
+        # set flags
+        self.__state_is_from_minimizer = False
+
+
+
+
+
+
---
+++ kafe2/test/core/test_nexus_fitter.py
@@ -0,0 +1,269 @@
+import numpy as np
+import six
+import unittest2 as unittest
+
+from kafe2.core.minimizers import AVAILABLE_MINIMIZERS
+from kafe2.core.fitters.nexus import Nexus, Parameter, Function
+from kafe2.core.fitters.nexus_fitter import NexusFitter, NexusFitterException
+
+
+class AbstractTestNexusFitter(object):
+
+    MINIMIZER = None
+
+    @staticmethod
+    def slsq(x, y, x_1, y_1, x_2, y_2):
+        return (
+            (x - x_1)**2 + (y - y_1)**2 +
+            (x - x_2)**2 + (y - y_2)**2
+        )
+
+    def setUp(self):
+        self.nexus = Nexus()
+
+        self._ref_xy_1 = (3, 9)
+        self._ref_xy_2 = (7, 11)
+        self._ref_xy = tuple(
+            0.5*(np.array(self._ref_xy_1) + np.array(self._ref_xy_2))
+        )
+        self._startval_x = 300
+        self._startval_y = -20
+
+        par_x_1, par_y_1 = (
+            self.nexus.add(
+                Parameter(self._ref_xy_1[0], name='x_1')
+            ),
+            self.nexus.add(
+                Parameter(self._ref_xy_1[1], name='y_1')
+            )
+        )
+        par_x_2, par_y_2 = (
+            self.nexus.add(
+                Parameter(self._ref_xy_2[0], name='x_2')
+            ),
+            self.nexus.add(
+                Parameter(self._ref_xy_2[1], name='y_2')
+            )
+        )
+
+        par_x, par_y = (
+            self.nexus.add(
+                Parameter(self._startval_x, name='x')
+            ),
+            self.nexus.add(
+                Parameter(self._startval_y, name='y')
+            )
+        )
+
+        self.nexus.add_function(self.slsq)
+
+        self.fitter = NexusFitter(
+            self.nexus,
+            parameters_to_fit=('x', 'y'),
+            parameter_to_minimize='slsq',
+            minimizer=self.MINIMIZER
+        )
+
+    def _assert_fit_results(self, xy_val=None):
+
+        xy_val = xy_val or self._ref_xy
+
+        for _par_name, _val in zip(('x', 'y'), xy_val):
+            self.assertAlmostEqual(
+                self.fitter.fit_parameter_values[_par_name],
+                _val,
+                places=3
+            )
+
+        _slsq_args = tuple(xy_val) + self._ref_xy_1 + self._ref_xy_2
+        self.assertAlmostEqual(
+            self.fitter.parameter_to_minimize_value,
+            self.slsq(*_slsq_args),
+            places=2
+        )
+
+    # -- init
+
+    def test_init_inexistent_fit_parameters_raise(self):
+        with self.assertRaises(NexusFitterException):
+            NexusFitter(
+                self.nexus,
+                parameters_to_fit=('bogus_parameter',),
+                parameter_to_minimize='slsq',
+            )
+
+    def test_init_inexistent_min_parameter_raise(self):
+        with self.assertRaises(NexusFitterException):
+            NexusFitter(
+                self.nexus,
+                parameters_to_fit=('x', 'y'),
+                parameter_to_minimize='bogus_parameter',
+            )
+
+    # -- basic interface
+
+    def test_simple_properties(self):
+        self.assertEqual(
+            self.fitter.parameters_to_fit,
+            ('x', 'y')
+        )
+        self.assertEqual(
+            self.fitter.parameter_to_minimize,
+            'slsq'
+        )
+        self.assertEqual(
+            self.fitter.fit_parameter_values,
+            {
+                'x': self._startval_x,
+                'y': self._startval_y,
+            }
+        )
+        self.assertEqual(
+            self.fitter.n_fit_par,
+            2
+        )
+        self._assert_fit_results(xy_val=[self._startval_x, self._startval_y])
+
+    def test_set_all_fit_parameter_values(self):
+        self.fitter.set_all_fit_parameter_values([7, 2])
+        self._assert_fit_results(xy_val=[7, 2])
+
+    def test_set_all_fit_parameter_values_length_mismatch_raise(self):
+        with self.assertRaises(NexusFitterException):
+            self.fitter.set_all_fit_parameter_values([7, 2, 30])
+        with self.assertRaises(NexusFitterException):
+            self.fitter.set_all_fit_parameter_values([2])
+
+    def test_set_fit_parameter_values(self):
+        self.fitter.set_fit_parameter_values(x=7, y=2)
+        self._assert_fit_results(xy_val=[7, 2])
+
+    def test_set_fit_parameter_values_unknown_raise(self):
+        with self.assertRaises(NexusFitterException):
+            self.fitter.set_fit_parameter_values(bogus_parameter=7)
+
+    def test_do_fit(self):
+        self.fitter.do_fit()
+        self._assert_fit_results()  # nominal fit results
+
+    def test_state_is_from_minimizer(self):
+        self.assertEqual(self.fitter.state_is_from_minimizer, False)
+        self.fitter.do_fit()
+        self.assertEqual(self.fitter.state_is_from_minimizer, True)
+        self.fitter.set_fit_parameter_values(x=7)
+        self.assertEqual(self.fitter.state_is_from_minimizer, False)
+
+    # -- parameter errors
+
+    def test_fit_parameter_errors(self):
+        self.fitter.do_fit()
+        self.assertAlmostEqual(
+            self.fitter.fit_parameter_errors[0],
+            1.0/np.sqrt(2)
+        )
+        self.assertAlmostEqual(
+            self.fitter.fit_parameter_errors[1],
+            1.0/np.sqrt(2)
+        )
+
+    def test_fit_parameter_cov_mat(self):
+        self.fitter.do_fit()
+        self.assertAlmostEqual(
+            self.fitter.fit_parameter_cov_mat[0, 0],
+            0.5
+        )
+
+    def test_fit_parameter_cor_mat(self):
+        self.fitter.do_fit()
+        self.assertAlmostEqual(
+            self.fitter.fit_parameter_cor_mat[0, 0],
+            1.0
+        )
+
+    def test_asymmetric_fit_parameter_errors(self):
+        self.fitter.do_fit()
+        self.assertAlmostEqual(
+            self.fitter.asymmetric_fit_parameter_errors[0][0],
+            -1.0/np.sqrt(2),
+            places=4
+        )
+        self.assertAlmostEqual(
+            self.fitter.asymmetric_fit_parameter_errors[0][1],
+            1.0/np.sqrt(2),
+            places=4
+        )
+
+    def test_asymmetric_fit_parameter_errors_if_calculated(self):
+        self.fitter.do_fit()
+        self.assertIs(
+            self.fitter.asymmetric_fit_parameter_errors_if_calculated,
+            None
+        )
+
+    # -- fixing & limiting parameters
+
+    def test_fix_parameter(self):
+        self.fitter.fix_parameter('x')
+        self.fitter.do_fit()
+        self._assert_fit_results(xy_val=[self._startval_x, self._ref_xy[1]])
+
+    def test_fix_parameter_to_value(self):
+        self.fitter.fix_parameter('x', 200)
+        self.fitter.do_fit()
+        self._assert_fit_results(xy_val=[200, self._ref_xy[1]])
+
+    def test_release_parameter(self):
+        self.fitter.fix_parameter('x')
+        self.fitter.do_fit()
+        self.fitter.release_parameter('x')
+        self.fitter.do_fit()
+        self._assert_fit_results()  # nominal fit results
+
+    def test_limit_parameter(self):
+        self.fitter.limit_parameter('x', (6, 10))
+        self.fitter.do_fit()
+        self._assert_fit_results(xy_val=[6, self._ref_xy[1]])
+
+    def test_unlimit_parameter(self):
+        self.fitter.limit_parameter('x', (6, 10))
+        self.fitter.do_fit()
+        self.fitter.unlimit_parameter('x')
+        self.fitter.do_fit()
+        self._assert_fit_results()  # nominal fit results
+
+
+    # -- profiles & contours
+
+    def test_profile_without_do_fit(self):
+        with self.assertRaises(NexusFitterException):
+            self.fitter.profile('x')
+
+    def test_profile(self):
+        self.fitter.do_fit()
+        p = self.fitter.profile('x', bins=20, bound=2)
+        self.assertEqual(
+            p.shape,
+            (2, 20)
+        )
+        self.assertAlmostEqual(
+            p[0][-1] - p[0][0],
+            4 * self.fitter.fit_parameter_errors[0]
+        )
+
+    def test_contour_without_do_fit(self):
+        with self.assertRaises(NexusFitterException):
+            self.fitter.contour('x', 'y')
+
+    def test_contour(self):
+        self.fitter.do_fit()
+        c = self.fitter.contour('x', 'y')
+        # TODO: test result?
+
+
+if 'scipy' in AVAILABLE_MINIMIZERS:
+    class TestNexusFitterScipy(AbstractTestNexusFitter, unittest.TestCase):
+        MINIMIZER = 'scipy'
+
+if 'iminuit' in AVAILABLE_MINIMIZERS:
+    class TestNexusFitterIMinuit(AbstractTestNexusFitter, unittest.TestCase):
+        MINIMIZER = 'iminuit'
