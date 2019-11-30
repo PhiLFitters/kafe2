30c178ac4bd7d23fa8bb09e4949ed3251af6c914
JohannesGaessler <johannesg@5d6.de>
2019-11-30 16:12:35 +0100

    Revert: Erroneously committed edit to linear regression example

--- examples/001_linear_regression/linear_regression.py
+++ examples/001_linear_regression/linear_regression.py
@@ -27,7 +27,7 @@ xy_data.add_simple_error(axis='y', err_val=0.4)
 
 # Create an XYFit object from the xy data container.
 # By default, a linear function f=a*x+b will be used as the model function.
-line_fit = XYFit(xy_data=xy_data, minimizer='scipy', x_error_algorithm='nonlinear')
+line_fit = XYFit(xy_data=xy_data)
 
 # Perform the fit: Find values for a and b that minimize the
 #     difference between the model function and the data.
@@ -41,14 +41,6 @@ plot = Plot(fit_objects=line_fit) # Create a kafe2 plot object.
 plot.plot(with_fit_info=True) # Do the plot.
 
 # Show the fit result.
-#plt.show()
+plt.show()
 
-from kafe2.test.tools import calculate_expected_fit_parameters_xy
-from kafe2.fit.util.function_library import linear_model, linear_model_derivative
-_expected_parameters = calculate_expected_fit_parameters_xy(
-    x_data=[1.0, 2.0, 3.0, 4.0], y_data=[2.3, 4.2, 7.5, 9.4], model_function=linear_model, y_error=[0.4] * 4,
-    initial_parameter_values=[1.0, 1.0],
-    x_error=[0.1] * 4, model_function_derivative=linear_model_derivative
-)
-print line_fit.parameter_values
-print _expected_parameters
+
