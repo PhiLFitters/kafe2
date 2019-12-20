0545edc83ad743f0903d23e60d55e52154329ee1
Daniel Savoiu <daniel.savoiu@cern.ch>
2019-12-20 13:58:20 +0100

    New submodule '_version_info' for single-sourcing version. Adapt setup.py.

--- kafe2/__init__.py
+++ kafe2/__init__.py
@@ -1,9 +1,10 @@
 import logging
 
+from ._version_info import __version__
+
 from .config import *
 from .fit import *
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 
-__version__ = "2.0.0.dev0"
--- setup.py
+++ setup.py
@@ -6,13 +6,10 @@ import os
 
 from setuptools import find_packages, setup
 
-__version__ = "0.1.0"
-
 def discover_kafe_tests():
     _tl = unittest.TestLoader()
     _ts = _tl.discover('kafe2/test', 'test_*.py')
     return _ts
-
 
 def read_local(filename):
     _path = os.path.join(os.path.dirname(__file__), filename)
@@ -21,11 +18,18 @@ def read_local(filename):
     else:
         return ""
 
+def get_version():
+    '''get kafe2 version without importing the whole package'''
+    _tmp_locals = {}
+    exec(read_local("kafe2/_version_info.py"), _tmp_locals)
+    return _tmp_locals['__version__']
+
 setup(
     name='kafe2',
-    version=__version__,
+    version=get_version(),
     description='Karlsruhe Fit Environment 2: a package for fitting and elementary data analysis',
-    long_description=read_local('README'),
+    long_description=read_local('README.rst'),
+    long_description_content_type="text/x-rst",
     author='Daniel Savoiu',
     author_email='daniel.savoiu@cern.ch',
     url='http://github.com/dsavoiu/kafe2',
@@ -33,7 +37,9 @@ setup(
     package_data={'kafe2': ['config/*.conf', 'config/*.yaml', 'config/*.yml', 'fit/tools/kafe2go']},
     scripts=['kafe2/fit/tools/kafe2go.py', 'kafe2/fit/tools/kafe2go'],
     test_suite='setup.discover_kafe_tests',
-    keywords="data analysis lab courses education students physics fitting minimization",
+    keywords=("kafe2 kit karlsruhe data analysis lab laboratory practical courses "
+              "education university students physics fitting minimization minimisation "
+              "regression parametric parameter estimation optimization optimisation"),
     license='GPL3',
     #TODO requirement versions
     install_requires=[
@@ -58,5 +64,9 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
