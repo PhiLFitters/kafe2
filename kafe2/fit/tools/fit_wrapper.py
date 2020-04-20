8117cbf850b234dd9c2e7ab8e9bf4aa78d19d748
Cedric Verstege <cedric.verstege@student.kit.edu>
2020-04-20 23:04:18 +0200

    [Tools] add minimizer to Fit wrapper

--- kafe2/fit/tools/fit_wrapper.py
+++ kafe2/fit/tools/fit_wrapper.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-from ..indexed import IndexedContainer, IndexedFit
 from ..histogram import HistContainer, HistFit
+from ..indexed import IndexedContainer, IndexedFit
 from ..unbinned import UnbinnedContainer, UnbinnedFit
 from ..xy import XYContainer, XYFit
-
 
 __all__ = ['Fit']
 
 
-def Fit(data_container, model_function=None):
+def Fit(data_container, model_function=None, minimizer=None):
     """A convenience wrapper for simple fit creation. For more control over the fit creation use the corresponding Fit
     classes.
 
@@ -17,6 +16,9 @@ def Fit(data_container, model_function=None):
                            used in the fit.
     :type data_container: IndexedContainer or HistContainer or UnbinnedContainer or XYContainer or list
     :param model_function: The model function used in the fit.
+    :param minimizer: The minimizer backend to use for the fit. This can be ``'scipy'``, ``'iminuit'`` or ``'tminuit'``,
+        depending on the installed backends.
+    :type minimizer: str or None
     """
     container_to_fit = {IndexedContainer: IndexedFit,
                         HistContainer: HistFit,
@@ -30,5 +32,6 @@ def Fit(data_container, model_function=None):
         raise TypeError("Unknown or unsupported data container. Supported types are {}".format(container_to_fit.keys()))
     # other errors will raise during creation of the fit object
     if model_function is None:
-        return fit_class(data_container)  # use default model function
-    return fit_class(data_container, model_function)
+        return fit_class(data_container, minimizer=minimizer)  # use default model function
+    # model function is not always called model_function e.g. histogram fit, don't use keyword!
+    return fit_class(data_container, model_function, minimizer=minimizer)
