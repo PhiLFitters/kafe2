c8b7c6fa4b21def4925300da68a73301f85499b7
Cedric Verstege <cedric.verstege@student.kit.edu>
2020-09-03 00:08:40 +0200

    [examples] add unbinned kafe2go example

--- examples/006_unbinned_fit/unbinned.py
+++ examples/006_unbinned_fit/unbinned.py
@@ -3,14 +3,16 @@
 kafe2 example: Unbinned Fit
 ================================
 
-An unbinned fit is needed, when there are too few data points to create a (good) histogram. If a histogram is created
-from too few data points, information can be lost or even changed, by changing the exact value of one data point to the
-range of a bin. With an unbinned likelihood fit it's still possible to fit the pdf to the data points, as the likelihood
-of each data point is fitted.
+An unbinned fit is needed, when there are too few data points to create a (good) histogram.
+If a histogram is created from too few data points, information can be lost or even changed,
+by changing the exact value of one data point to the range of a bin.
+With an unbinned likelihood fit it's still possible to fit the pdf to the data points, as the
+likelihood of each data point is fitted.
 
-In this example the decay time of muons is calculated. Tha data was collected using the Kamiokanne experiment (a
-water-Cherenkov detector with photomultiplier readout). For more information on the experimental setup visit
-https://github.com/GuenterQuast/picoCosmo. A visual representation of the data can be found here:
+In this example the decay time of muons is calculated. Tha data was collected using the Kamiokanne
+experiment (a water-Cherenkov detector with photomultiplier readout). For more information on the
+experimental setup visit https://github.com/GuenterQuast/picoCosmo.
+A visual representation of the data can be found here:
 https://github.com/GuenterQuast/picoCosmo/blob/master/doc/dpFigs_Kanne.pdf
 """
 
@@ -23,8 +25,8 @@ import matplotlib.pyplot as plt
 
 def pdf(t, tau=2.2, fbg=0.1, a=1., b=9.75):
     """
-    Probability density function for the decay time of a myon using the Kamiokanne-Experiment. The pdf is normed for the
-    interval (a, b).
+    Probability density function for the decay time of a myon using the Kamiokanne-Experiment.
+    The pdf is normed for the interval (a, b).
 
     :param t: decay time
     :param fbg: background
@@ -44,11 +46,13 @@ dT = np.loadtxt(infile)
 
 data = UnbinnedContainer(dT)  # create the kafe data object
 data.label = 'lifetime measurements'
-data.axis_labels = ['life time ' r'$\tau$' '(µs)', 'Density']
+data.axis_labels = ['life time $\\tau$ (µs)', 'Density']
 
-fit = UnbinnedFit(data=data, model_density_function=pdf)  # create the fit object and set the pdf for the fit
+# create the fit object and set the pdf for the fit
+fit = UnbinnedFit(data=data, model_density_function=pdf)
 
-# Fix the parameters a and b. Those are responsible for the normalization of the pdf for the range (a, b).
+# Fix the parameters a and b.
+# Those are responsible for the normalization of the pdf for the range (a, b).
 fit.fix_parameter("a", 1)
 fit.fix_parameter("b", 11.5)
 # constrain parameter fbg to avoid unphysical region
@@ -68,7 +72,9 @@ fit.report(asymmetric_parameter_errors=True)  # print a fit report to the termin
 plot = Plot(fit)  # create a plot object
 plot.plot(fit_info=True, asymmetric_parameter_errors=True)  # plot the data and the fit
 
-cpf = ContoursProfiler(fit, profile_subtract_min=False)  # Optional: create a contours profile
-cpf.plot_profiles_contours_matrix(parameters=['tau', 'fbg'])  # Optional: plot the contour matrix for tau and fbg
+# Optional: create a contours profile
+cpf = ContoursProfiler(fit, profile_subtract_min=False)
+# Optional: plot the contour matrix for tau and fbg
+cpf.plot_profiles_contours_matrix(parameters=['tau', 'fbg'])
 
 plt.show()  # show the plot(s)
