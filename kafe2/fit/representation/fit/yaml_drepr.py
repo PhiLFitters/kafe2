473b84febcab236178058e84c81b778b2b7be08d
Cedric Verstege <cedric.verstege@student.kit.edu>
2020-09-24 12:12:14 +0200

    [repr] cleanup and simplify fit drepr

--- kafe2/fit/representation/fit/yaml_drepr.py
+++ kafe2/fit/representation/fit/yaml_drepr.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-from .._base import DReprError
-from .._yaml_base import YamlWriterMixin, YamlReaderMixin, YamlReaderException
 from ._base import FitDReprBase
 from .. import _AVAILABLE_REPRESENTATIONS
-from ....fit import IndexedFit, HistFit, UnbinnedFit, XYFit
+from .._base import DReprError
+from .._yaml_base import YamlWriterMixin, YamlReaderMixin, YamlReaderException
+from ..constraint.yaml_drepr import ConstraintYamlReader, ConstraintYamlWriter
 from ..container.yaml_drepr import DataContainerYamlReader, DataContainerYamlWriter
 from ..model.yaml_drepr import ParametricModelYamlReader, ParametricModelYamlWriter
-from ..constraint.yaml_drepr import ConstraintYamlReader, ConstraintYamlWriter
+from ....fit import IndexedFit, HistFit, UnbinnedFit, XYFit
 from ....tools import get_compact_representation
 
 __all__ = ['FitYamlWriter', 'FitYamlReader']
@@ -24,7 +24,8 @@ class FitYamlWriter(YamlWriterMixin, FitDReprBase):
         _preface_comment = super(FitYamlWriter, self)._get_preface_comment()
         _did_fit = self._kafe_object.did_fit
         if not _did_fit:
-            _preface_comment += '\n# WARNING: No fit has been performed as of yet. Did you forget to run fit.do_fit()?\n'
+            _preface_comment += "\n# WARNING: No fit has been performed as of yet. " \
+                                "Did you forget to run fit.do_fit()?\n"
         _preface_comment += '\n'
         if self._kafe_object.model_count == 1:
             _preface_comment += "# Model function: %s\n" % self._kafe_object._model_function.formatter.get_formatted(
@@ -46,11 +47,11 @@ class FitYamlWriter(YamlWriterMixin, FitDReprBase):
         if _did_fit:
             _cost = self._kafe_object.cost_function_value
             _ndf = self._kafe_object.ndf
-            _round_cost_sig = max(2, int(-np.floor(np.log(_cost)/np.log(10))) + 1)
+            _round_cost_sig = max(2, int(-np.floor(np.log(_cost) / np.log(10))) + 1)
             _rounded_cost = round(_cost, _round_cost_sig)
             _preface_comment += "# Cost: %s\n" % _rounded_cost
             _preface_comment += '# ndf: %s\n' % _ndf
-            _round_cost_per_ndf_sig = max(2, int(-np.floor(np.log(_cost / _ndf)/np.log(10))) + 1)
+            _round_cost_per_ndf_sig = max(2, int(-np.floor(np.log(_cost / _ndf) / np.log(10))) + 1)
             _preface_comment += "# Cost/ndf: %s\n\n" % round(_cost / _ndf, _round_cost_per_ndf_sig)
 
             # If asymmetric parameters errors were not calculated, check the loaded result dict
@@ -81,7 +82,7 @@ class FitYamlWriter(YamlWriterMixin, FitDReprBase):
         _yaml_doc['dataset'] = DataContainerYamlWriter._make_representation(fit.data_container)
         _yaml_doc['parametric_model'] = ParametricModelYamlWriter._make_representation(fit._param_model)
 
-        #TODO cost function
+        # TODO cost function
 
         _yaml_doc['minimizer'] = fit._minimizer
         _yaml_doc['minimizer_kwargs'] = fit._minimizer_kwargs
@@ -105,7 +106,7 @@ class FitYamlWriter(YamlWriterMixin, FitDReprBase):
 
 
 class FitYamlReader(YamlReaderMixin, FitDReprBase):
-    
+
     def __init__(self, input_io_handle):
         super(FitYamlReader, self).__init__(
             input_io_handle=input_io_handle,
@@ -116,16 +117,15 @@ class FitYamlReader(YamlReaderMixin, FitDReprBase):
         if fit_class in (HistFit, XYFit):
             return ['dataset']
         return ['dataset', 'parametric_model']
-    
-    @classmethod
-    def _modify_yaml_doc(cls, yaml_doc, kafe_object_class):
-        return yaml_doc
-    
+
     @classmethod
     def _get_subspace_override_dict(cls, fit_class):
         _override_dict = {'model_parameters': 'parametric_model',
                           'arg_formatters': 'parametric_model',
-                          'model_function_formatter': 'parametric_model'}
+                          'model_function_formatter': 'parametric_model',
+                          'expression_string': 'parametric_model',
+                          'latex_expression_string': 'parametric_model',
+                          }
 
         if fit_class is HistFit:
             _override_dict['n_bins'] = ['dataset', 'parametric_model']
@@ -136,8 +136,6 @@ class FitYamlReader(YamlReaderMixin, FitDReprBase):
             _override_dict['model_density_function'] = 'parametric_model'
             _override_dict['model_density_function_name'] = 'parametric_model'
             _override_dict['latex_model_density_function_name'] = 'parametric_model'
-            _override_dict['expression_string'] = 'parametric_model'
-            _override_dict['latex_expression_string'] = 'parametric_model'
         elif fit_class is IndexedFit:
             _override_dict['data'] = 'dataset'
             _override_dict['errors'] = 'dataset'
@@ -146,15 +144,11 @@ class FitYamlReader(YamlReaderMixin, FitDReprBase):
             _override_dict['latex_model_function_name'] = 'parametric_model'
             _override_dict['index_name'] = 'parametric_model'
             _override_dict['latex_index_name'] = 'parametric_model'
-            _override_dict['expression_string'] = 'parametric_model'
-            _override_dict['latex_expression_string'] = 'parametric_model'
         elif fit_class is UnbinnedFit:
             _override_dict['data'] = ['dataset', 'parametric_model']
             _override_dict['model_function'] = 'parametric_model'
             _override_dict['model_function_name'] = 'parametric_model'
             _override_dict['latex_model_function_name'] = 'parametric_model'
-            _override_dict['expression_string'] = 'parametric_model'
-            _override_dict['latex_expression_string'] = 'parametric_model'
         elif fit_class is XYFit:
             _override_dict['x_data'] = ['dataset', 'parametric_model']
             _override_dict['y_data'] = 'dataset'
@@ -163,8 +157,6 @@ class FitYamlReader(YamlReaderMixin, FitDReprBase):
             _override_dict['model_function'] = 'parametric_model'
             _override_dict['model_function_name'] = 'parametric_model'
             _override_dict['latex_model_function_name'] = 'parametric_model'
-            _override_dict['expression_string'] = 'parametric_model'
-            _override_dict['latex_expression_string'] = 'parametric_model'
         else:
             raise YamlReaderException("Unknown fit type")
         # override labels for every fit type
@@ -173,7 +165,7 @@ class FitYamlReader(YamlReaderMixin, FitDReprBase):
         _override_dict['x_label'] = 'dataset'
         _override_dict['y_label'] = 'dataset'
         return _override_dict
-    
+
     @classmethod
     def _convert_yaml_doc_to_object(cls, yaml_doc):
         # -- determine fit class from type
@@ -183,42 +175,18 @@ class FitYamlReader(YamlReaderMixin, FitDReprBase):
         _data = DataContainerYamlReader._make_object(yaml_doc.pop('dataset'), default_type=_fit_type)
         _parametric_model_entry = yaml_doc.pop('parametric_model', None)
         if _parametric_model_entry:
-            _read_parametric_model = ParametricModelYamlReader._make_object(_parametric_model_entry, default_type=_fit_type)
+            _read_parametric_model = ParametricModelYamlReader._make_object(_parametric_model_entry,
+                                                                            default_type=_fit_type)
             _read_model_function = _read_parametric_model._model_function_object
         else:
             _read_parametric_model = None
             _read_model_function = None
-        #TODO cost function
+        # TODO: cost function
         _minimizer = yaml_doc.pop('minimizer', None)
         _minimizer_kwargs = yaml_doc.pop('minimizer_kwargs', None)
-        if _class is HistFit:
-            _fit_object = HistFit(
-                data=_data,
-                model_density_function=_read_model_function,
-                minimizer=_minimizer,
-                minimizer_kwargs=_minimizer_kwargs
-            )
-        elif _class is IndexedFit:
-            _fit_object = IndexedFit(
-                data=_data,
-                model_function=_read_model_function,
-                minimizer=_minimizer,
-                minimizer_kwargs=_minimizer_kwargs
-            )
-        elif _class is UnbinnedFit:
-            _fit_object = UnbinnedFit(
-                data=_data,
-                model_density_function=_read_model_function,
-                minimizer=_minimizer,
-                minimizer_kwargs=_minimizer_kwargs
-            )
-        elif _class is XYFit:
-            _fit_object = XYFit(
-                xy_data=_data,
-                model_function=_read_model_function,
-                minimizer=_minimizer,
-                minimizer_kwargs=_minimizer_kwargs
-            )
+        # change fit kwargs for different fit types if necessary
+        _fit_kwargs = dict(minimizer=_minimizer, minimizer_kwargs=_minimizer_kwargs)
+        _fit_object = _class(_data, _read_model_function, **_fit_kwargs)
 
         if _read_parametric_model is not None:
             _fit_object._param_model = _read_parametric_model
