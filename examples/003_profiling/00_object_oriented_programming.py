54afa25194ff0c963f4030d46d107313b1c1e9a7
JohannesGaessler <johannesg@5d6.de>
2022-10-12 21:18:49 +0200

    Added a mini-example to introduce OOP

---
+++ examples/003_profiling/00_object_oriented_programming.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+kafe2 example: Object Oriented Programming
+==========================================
+
+Until now fits were performed using the methods kafe2.xy_fit and kafe2.plot. These methods provide
+pre-configured pipelines for fitting models to xy data and plotting the results. Internally they use
+objects (in the programming sense) which represent things like data or fits as a whole. From now on
+the examples will use the objects directly. This is slightly more complicated but it also provides
+greater flexibility.
+
+This example serves as an introduction to kafe2 objects. The implementation is mostly comparable to
+001_line_fit/line_fit.py
+"""
+
+from kafe2 import XYContainer, Fit, Plot
+
+# Create an XYContainer object to hold the xy data for the fit:
+xy_data = XYContainer(x_data=[1.0, 2.0, 3.0, 4.0],
+                      y_data=[2.3, 4.2, 7.5, 9.4])
+# x_data and y_data are combined depending on their order.
+# The above translates to the points (1.0, 2.3), (2.0, 4.2), (3.0, 7.5), and (4.0, 9.4).
+
+# Important: Specify uncertainties for the data:
+xy_data.add_error(axis='x', err_val=0.1)
+xy_data.add_error(axis='y', err_val=0.4)
+
+xy_data.label = 'Data'  # How the data is called in plots
+
+# Create an XYFit object from the xy data container.
+# By default, a linear function f=a*x+b will be used as the model function.
+line_fit = Fit(data=xy_data)
+
+# Perform the fit: Find values for a and b that minimize the
+#     difference between the model function and the data.
+line_fit.do_fit()  # This will throw a warning if no errors were specified.
+
+# Optional: Print out a report on the fit results on the console.
+line_fit.report()
+# With kafe2.xy_fit this gets printed to a file.
+
+# Optional: Create a plot of the fit results using Plot.
+plot = Plot(fit_objects=line_fit)  # Create a kafe2 plot object.
+plot.x_label = 'x'  # Set x axis label.
+plot.y_label = 'y'  # Set y axis label.
+plot.plot()  # Do the plot.
+
+plot.save()  # Saves the plot to file 'fit.png' .
+# plot.save('my_fit.pdf')  # Saves the plot to a different file / with a different file extension.
+
+# Show the fit result.
+plot.show()  # Just a convenience wrapper for matplotlib.pyplot.show() .
+# NOTE: Calling matplotlib.pyplot.show() closes all figures by default so call this AFTER saving.
+
+# Alternatively you could still use the function kafe.plot like this:
+# from kafe2 import plot  # Notice that "plot" is not capitalized. "Plot" is the plot object.
+# kafe2.plot(line_fit)  # Pass the fit object to the plot function.
--- examples/003_profiling/01_non_linear_fit.py
+++ examples/003_profiling/01_non_linear_fit.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
 """
-kafe2 example: non-linear fit
-=============================
-Very often, when the fit model is a non-linear function of the parameters, the chi2 function is not parabolic around
-the minimum. A very common example of such a case is an exponential function parametrized as shown in this example.
-In the case of a nonlinear fit, the minimum of a chi2 cost function is not longer shaped like a parabola (with a model
-parameter on the x axis and chi2 on the y axis). Now, you might be wondering why you should care about the shape of the
-chi2 function. The reason why it's important is that the common notation of par_value+-par_error for fit results is only
-valid for a parabola-shaped cost function. If your likelihood function is distorted it will also affect your fit
-results!
+kafe2 example: nonlinear fit
+============================
+Very often, when the fit model is a nonlinear function of the parameters, the chi2 function is not
+parabolic around the minimum. A very common example of such a case is an exponential function
+parametrized as shown in this example. In the case of a nonlinear fit, the minimum of a chi2 cost
+function is not longer shaped like a parabola (with a model parameter on the x axis and chi2 on the
+y axis). Now, you might be wondering why you should care about the shape of the chi2 function. The
+reason why it's important is that the common notation of par_value+-par_error for fit results is
+only valid for a parabola-shaped cost function. If your likelihood function is distorted it will
+also affect your fit results!
 
-Luckily nonlinear fits oftentimes still produce meaningful fit results as long as the distortion is not too big - you
-just need to be more careful during the evaluation of your fit results. A common approach for handling nonlinearity is
-to trace the profile of the cost function (in this case chi2) in either direction of the cost function minimum and find
-the points at which the cost function value has increased by a specified amount relative to the cost function minimum.
-In other words, two cuts are made on either side of the cost function minimum at a specified height. The two points
-found with this approach span a confidence interval for the fit parameter around the cost function minimum. The
-confidence level of the interval depends on how high you set the cuts for the cost increase relative to the cost
-function minimum. The one sigma interval described by conventional parameter errors is achieved by a cut at the fit
-minimum plus 1^2=1 and has a confidence level of about 68%. The two sigma interval is achieved by a cut at the fit
-minimum plus 2^2=4 and has a confidence level of about 95%, and so on. The one sigma interval is commonly described by
-what is called asymmetric errors: the interval limits are described relative to the cost function minimum as
-par_value+par_err_up-par_err_down.
+Luckily nonlinear fits oftentimes still produce meaningful fit results as long as the distortion
+is not too big - you just need to be more careful during the evaluation of your fit results. A
+common approach for handling nonlinearity is to trace the profile of the cost function (in this case
+chi2) in either direction of the cost function minimum and find the points at which the cost
+function value has increased by a specified amount relative to the cost function minimum. In other
+words, two cuts are made on either side of the cost function minimum at a specified height. The two
+points found with this approach span a confidence interval for the fit parameter around the cost
+function minimum. The confidence level of the interval depends on how high you set the cuts for the
+cost increase relative to the cost function minimum. The one sigma interval described by
+conventional parameter errors is achieved by a cut at the fit minimum plus 1^2=1 and has a
+confidence level of about 68%. The two sigma interval is achieved by a cut at the fit minimum plus
+2^2=4 and has a confidence level of about 95%, and so on. The one sigma interval is commonly
+described by what is called asymmetric errors: the interval limits are described relative to the
+cost function minimum as par_value+par_err_up-par_err_down.
+
+Note: from this point on the examples use kafe2 objects rather than functions like kafe2.xy_fit.
+For an introduction to kafe2 objects look at the file 00_object_oriented_programming.py
 """
 
 import numpy as np
@@ -31,13 +37,13 @@ def exponential(x, A_0=1, tau=1):
     return A_0 * np.exp(-x/tau)
 
 # define the data as simple Python lists
-x = [8.018943e-01, 1.839664e+00, 1.941974e+00, 1.276013e+00, 2.839654e+00, 3.488302e+00, 3.775855e+00, 4.555187e+00,
-     4.477186e+00, 5.376026e+00]
+x = [8.018943e-01, 1.839664e+00, 1.941974e+00, 1.276013e+00, 2.839654e+00, 3.488302e+00,
+     3.775855e+00, 4.555187e+00, 4.477186e+00, 5.376026e+00]
 xerr = 3.000000e-01
-y = [2.650644e-01, 1.472682e-01, 8.077234e-02, 1.850181e-01, 5.326301e-02, 1.984233e-02, 1.866309e-02, 1.230001e-02,
-     9.694612e-03, 2.412357e-03]
-yerr = [1.060258e-01, 5.890727e-02, 3.230893e-02, 7.400725e-02, 2.130520e-02, 7.936930e-03, 7.465238e-03, 4.920005e-03,
-        3.877845e-03, 9.649427e-04]
+y = [2.650644e-01, 1.472682e-01, 8.077234e-02, 1.850181e-01, 5.326301e-02, 1.984233e-02,
+     1.866309e-02, 1.230001e-02, 9.694612e-03, 2.412357e-03]
+yerr = [1.060258e-01, 5.890727e-02, 3.230893e-02, 7.400725e-02, 2.130520e-02, 7.936930e-03,
+        7.465238e-03, 4.920005e-03, 3.877845e-03, 9.649427e-04]
 
 # create a fit object from the data arrays
 fit = Fit(data=[x, y], model_function=exponential)
@@ -49,7 +55,7 @@ fit.report(asymmetric_parameter_errors=True)  # print a report with asymmetric u
 
 # Optional: create a plot
 plot = Plot(fit)
-plot.plot(asymmetric_parameter_errors=True, ratio=True)  # add the ratio data/function and asymmetric errors
+plot.plot(asymmetric_parameter_errors=True, ratio=True)  # ratio = data / model function
 
 # Optional: create the contours profiler
 cpf = ContoursProfiler(fit)
